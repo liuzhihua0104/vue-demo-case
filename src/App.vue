8d61c7676b8f8749d0fc9833071d3bdb25a18085
liuzhihua <liuzihhuarose@126.com>
2022-02-08 11:19:17 +0800

    删除不用的初始文件，添加拖拽页面

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,49 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/vuedraggable">vuedraggable拖拽</router-link> |
+      <router-link to="/turntable">turntable大转盘</router-link>
     </div>
-    <router-view />
+        <router-view />
+
   </div>
 </template>
 
 <style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  #nav {
+    padding: 15px;
 
-#nav {
-  padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+    router-view {
+      flex: 1;
+      width: 100%;
+      background: green;
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/vuedraggable',
+    name: 'VueDraggable',
+    // component: Home
+    component: () => import(/* webpackChunkName: "about" */ '../views/Vuedraggable.vue')
+
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/turntable',
+    name: 'Turntable',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Turntable.vue')
+  }
 ];
 
 const router = new VueRouter({
